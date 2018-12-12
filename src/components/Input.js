90339c7ef69701aa5e391e648aa60ea8f9823014
mack-wang <641212003@qq.com>
2018-12-12 09:24:23 +0800

    开发input组件中

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import { isEmpty } from '../utils/util'
 
 class Input extends Component {
     static type = 'Input'
@@ -15,16 +16,56 @@ class Input extends Component {
       onChange: PropTypes.func,
       disabled: PropTypes.bool,
       clearable: PropTypes.bool,
+      defaultValue: PropTypes.any,
+      value: PropTypes.any,
+      prefixIcon: PropTypes.string,
+      suffixIcon: PropTypes.string,
     }
 
     onChange = (e) => {
-      console.log(e)
+      if (this.props.onChange) {
+        this.props.onChange(e.target.value)
+      }
+    }
+
+    onClear = () => {
+      if (this.props.onChange) {
+        this.props.onChange('')
+      }
     }
 
     render() {
       const {
-        clearable, disabled, ...otherProps
+        clearable, disabled, prefixIcon, suffixIcon, children, ...otherProps
       } = this.props
+
+      if ('value' in this.props) {
+        delete this.props.defaultValue
+      }
+
+      let prepend
+      let append
+      if (children) {
+        if (children.length) {
+          children.forEach((item) => {
+            if (item.props.slot === 'prepend') {
+              prepend = item
+            }
+
+            if (item.props.slot === 'append') {
+              append = item
+            }
+          })
+        } else {
+          if (children.props.slot === 'prepend') {
+            prepend = children
+          }
+
+          if (children.props.slot === 'append') {
+            append = children
+          }
+        }
+      }
 
       return (
         <div className={classNames({
@@ -32,12 +73,22 @@ class Input extends Component {
           disabled,
         })}
         >
+          { prepend }
+          { prefixIcon && <span className="prefix icon"><i className={`iconfont ${prefixIcon}`} /></span>}
           <input
+            className={classNames({
+              prefix: prefixIcon,
+              suffix: suffixIcon,
+              prepend,
+              append,
+            })}
             {...otherProps}
             disabled={disabled}
             onChange={this.onChange}
           />
-          { clearable && <span className="clearable"><i className="iconfont icon-close-circle" onClick={this.onClear} /></span>}
+          { suffixIcon && <span className="suffix icon"><i className={`iconfont ${suffixIcon}`} /></span>}
+          { clearable && !isEmpty(otherProps.value) && <span className="clearable"><i className="iconfont icon-close-circle" onClick={this.onClear} /></span> }
+          { append }
         </div>
       )
     }
--- src/example/InputExample.js
+++ src/example/InputExample.js
@@ -2,20 +2,45 @@ import React, { Component } from 'react'
 import Input from '../components/Input'
 
 class InputExample extends Component {
-  render() {
-    return (
-      <div className="web-example">
-        <div className="web-title">输入框</div>
-        <Input placeholder="请输入用户名" />
+    state = {
+      value1: '',
+    }
 
-        <div className="web-title">禁用</div>
-        <Input disabled placeholder="请输入用户名" />
+    onChange1 = (value) => {
+      this.setState({ value1: value })
+    }
 
-        <div className="web-title">可清除</div>
-        <Input placeholder="请输入用户名" clearable />
-      </div>
-    )
-  }
+    render() {
+      const { value1 } = this.state
+      return (
+        <div className="web-example">
+          <div className="web-title">输入框</div>
+          <Input placeholder="请输入用户名" />
+
+          <div className="web-title">禁用</div>
+          <Input disabled placeholder="请输入用户名" />
+
+          <div className="web-title">可清除</div>
+          <Input placeholder="请输入用户名" clearable onChange={this.onChange1} value={value1} />
+
+          <div className="web-title">图标</div>
+          <Input placeholder="请输入用户名" prefixIcon="icon-user" />
+          <Input placeholder="请输入用户名" suffixIcon="icon-user" />
+
+          <div className="web-title">组合</div>
+          <Input placeholder="请输入用户名" prefixIcon="icon-user">
+            <div className="yu-button prepend" slot="prepend">确认</div>
+          </Input>
+          <Input placeholder="请输入用户名" prefixIcon="icon-user">
+            <div className="yu-button append" slot="append">确认</div>
+          </Input>
+          <Input placeholder="请输入用户名" prefixIcon="icon-user">
+            <div className="yu-button prepend" slot="prepend">确认</div>
+            <div className="yu-button append" slot="append">确认</div>
+          </Input>
+        </div>
+      )
+    }
 }
 
 export default InputExample
